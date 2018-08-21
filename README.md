f88fd1c4ad7fc0c2ad279bd730eefe54a1ae091d
codemzy <codemzy@gmail.com>
2018-08-21 13:40:47 +0000

    🔦🎮 Checkpoint ./index.html:340930/394 ./style.css:340930/58 ./script.js:340930/293 ./README.md:340930/2

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Word Clock
 =================
 
 A date and time clock in words.
+
+
---
+++ script.js
@@ -0,0 +1,7 @@
+var date = Date.now();
+
+function setTime() {
+  var now = new Date();
+  var mins = now.getMinutes();
+  var hour = now.getHours();
+};
