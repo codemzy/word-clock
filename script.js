89a8f23aaf09c5d59bf4d3ee77f1dd2ee3501a04
codemzy <codemzy@gmail.com>
2018-08-21 14:02:29 +0000

    🍬🐜 Checkpoint ./script.js:340930/376 ./index.html:340930/145

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var date = Date.now();
 
+var numberWords = ['zero', 'one', 'two', 'three', 'four', 'five'];
+
 function getTime() {
   var now = new Date();
   var mins = now.getMinutes();
@@ -9,7 +11,9 @@ function getTime() {
 
 function setTime() {
   var time = getTime();
-  
+  var hour = time.hours <= 12 ? time.hours : time.hours - 12; // change from 24 hour
+  // set hour
+  document.querySelector('#hours #' + numberWords[hour]).classList.add('active');
 }
 
 
